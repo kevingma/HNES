37d91b3b3c06bd5eb4f26e1c7f0ba512006cf138
kevingma <akevingma@gmail.com>
2026-04-09 10:38:10 -0500

    init

--- background.js
+++ background.js
@@ -1,41 +1,84 @@
-// Add event listeners
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('REQUEST', request.method, request)
-  if (request.method == "getAllLocalStorage") {
-    sendResponse({data: localStorage});
-  }
-  else if (request.method == "getLocalStorage") {
-    sendResponse({data: localStorage[request.key]});
-  }
-  else if (request.method == "setLocalStorage") {
-    localStorage[request.key] = request.value;
-    sendResponse({});
-  }
-  else if (request.method == "getUserData") {
-    var data = getUserData(request.usernames);
-    sendResponse({ data: data });
-  }
-  else {
-    sendResponse({});
-  }
-});
-
-function getUserData(usernames) {
+function getUserData(usernames, items) {
   var results = {};
   for (var i = 0; i < usernames.length; i++) {
-    var key = usernames[i],
-        value = localStorage[key];
-    results[key] = value;
+    var key = usernames[i];
+    results[key] = items[key];
   }
   return results;
 }
 
-//expire old entries
-(function() {
-  for (i=0; i<localStorage.length; i++) {
-    var info = JSON.parse(localStorage[localStorage.key(i)]);
-    var now = new Date().getTime();
-    if (now > info.expire)
-      localStorage.removeItem(localStorage.key(i));
+function pruneExpiredEntries(callback) {
+  chrome.storage.local.get(null, function(items) {
+    var expiredKeys = [];
+    var now = Date.now();
+    var keys = Object.keys(items);
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var value = items[key];
+
+      if (typeof value !== 'string') {
+        continue;
+      }
+
+      try {
+        var info = JSON.parse(value);
+        if (info && typeof info.expire === 'number' && now > info.expire) {
+          expiredKeys.push(key);
+        }
+      } catch (error) {
+        // Ignore non-JSON and legacy values.
+      }
+    }
+
+    if (!expiredKeys.length) {
+      callback();
+      return;
+    }
+
+    chrome.storage.local.remove(expiredKeys, callback);
+  });
+}
+
+function handleMessage(request, sendResponse) {
+  if (request.method == "getAllLocalStorage") {
+    chrome.storage.local.get(null, function(items) {
+      sendResponse({ data: items });
+    });
   }
+  else if (request.method == "getLocalStorage") {
+    chrome.storage.local.get([request.key], function(items) {
+      sendResponse({ data: items[request.key] });
+    });
+  }
+  else if (request.method == "setLocalStorage") {
+    chrome.storage.local.set({
+      [request.key]: request.value
+    }, function() {
+      sendResponse({});
+    });
+  }
+  else if (request.method == "getUserData") {
+    chrome.storage.local.get(request.usernames, function(items) {
+      sendResponse({ data: getUserData(request.usernames, items) });
+    });
+  }
+  else {
+    sendResponse({});
+  }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  pruneExpiredEntries(function() {
+    handleMessage(request, sendResponse);
+  });
+  return true;
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  pruneExpiredEntries(function() {});
+});
+
+chrome.runtime.onStartup.addListener(function() {
+  pruneExpiredEntries(function() {});
 });
